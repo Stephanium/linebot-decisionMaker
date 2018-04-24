05f881cdbd67523708b0e0ffabc1d67fc8336933
Stephanium <ste841114@gmail.com>
2018-04-24 15:24:26 +0800

    text update

--- actions/呼叫.js
+++ actions/呼叫.js
@@ -1,3 +1,10 @@
 module.exports = async context => {
-  await context.replyText(`你在叫我嗎？啾咪>///<`);
+  const 回應 = [
+    '你在叫我嗎？啾咪>///<',
+    '喲！！！\n我來了(￣▽￣)/',
+    '嗨嗨嗨~ 怎麼了？\n又有選擇障礙了嗎？',
+  ];
+  const r = Math.floor(Math.random() * 回應.length);
+  const n = 回應[r];
+  await context.replyText(`${n}`);
 };
